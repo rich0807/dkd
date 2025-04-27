5107f1cf70e2d7492c25de0ee02ec1c5066b6930
rich0807 <3428773947@qq.com>
2025-04-27 17:49:43 +0800

    feat:项目1.0

---
+++ bin/redis01.bat
@@ -0,0 +1,3 @@
+@echo off
+cd /d "D:\Major\xiangmu\Redis-x64-3.2.100"
+start redis-server.exe
---
+++ dkd-manage/src/main/java/com/dkd/manage/domain/vo/RegionVo.java
@@ -0,0 +1,9 @@
+package com.dkd.manage.domain.vo;
+
+import com.dkd.manage.domain.Region;
+import lombok.Data;
+
+@Data
+public class RegionVo extends Region {
+    private int nodeCount;
+}

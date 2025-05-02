534a4ea2df63d88d43d75734ec0b42e09f3ca063
rich0807 <3428773947@qq.com>
2025-05-02 12:11:59 +0800

    fix:add dkd-vue

---
+++ dkd-vue/dkd-vue/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <router-view/>
+</template>
+
+<script setup>
+import useSettingsStore from '@/store/modules/settings'
+import {handleThemeStyle} from '@/utils/theme'
+// 初始化主题样式
+
+onMounted(() => {
+  nextTick(() => {
+    // 初始化主题样式
+    handleThemeStyle(useSettingsStore().theme)
+  })
+})
+</script>

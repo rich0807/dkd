5f949423be1719cc1ca5892fd20bb13903f1ea0e
rich0807 <3428773947@qq.com>
2025-05-06 12:28:34 +0800

    fix:人员列表的改造-同步存储。

--- dkd-manage/src/main/java/com/dkd/manage/service/impl/RegionServiceImpl.java
+++ dkd-manage/src/main/java/com/dkd/manage/service/impl/RegionServiceImpl.java
@@ -3,11 +3,13 @@ package com.dkd.manage.service.impl;
 import java.util.List;
 import com.dkd.common.utils.DateUtils;
 import com.dkd.manage.domain.vo.RegionVo;
+import com.dkd.manage.mapper.EmpMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.dkd.manage.mapper.RegionMapper;
 import com.dkd.manage.domain.Region;
 import com.dkd.manage.service.IRegionService;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * 区域管理Service业务层处理
@@ -18,6 +20,9 @@ import com.dkd.manage.service.IRegionService;
 @Service
 public class RegionServiceImpl implements IRegionService 
 {
+    @Autowired
+private EmpMapper empMapper;
+
     @Autowired
     private RegionMapper regionMapper;
 
@@ -60,17 +65,22 @@ public class RegionServiceImpl implements IRegionService
 
     /**
      * 修改区域管理
-     * 
+     *
      * @param region 区域管理
      * @return 结果
      */
+    @Transactional(rollbackFor = Exception.class)
     @Override
     public int updateRegion(Region region)
     {
+        // 先更新区域信息
         region.setUpdateTime(DateUtils.getNowDate());
-        return regionMapper.updateRegion(region);
-    }
+        int result = regionMapper.updateRegion(region);
 
+        // 同步更新员工表区域名称
+        empMapper.updateByRegionId(region.getRegionName(),region.getId());
+        return result;
+    }
     /**
      * 批量删除区域管理
      * 
